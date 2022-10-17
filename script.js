32fd8051a0a124cee230f8b296ae3d7903a16c27
John Temiloluwa <johntemiloluwa26@gmail.com>
2022-10-17 15:04:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const {dataStore} = require("./txn")
+//console.log(dataStore);
+
+//get all transactions
+function getAllTransactions () {
+   return dataStore
+}
+
+
+// Add a new transaction
+function addNewTransactions (trans) {
+   dataStore.push(trans)
+}
+
+
+// Update an existing Transaction
+function updateTransaction (position, trans) {
+    return {...dataStore[position], trans}  
+}
+//const a = updateTransaction(0, 'key: 6')
+//console.log(a);
+
+
+//delete a transaction
+function deleteTrans (trans) {
+  return dataStore.filter((cur, i) => i !== trans)
+}
+
+
+//get transaction by ID
+function getTranctionsID (trans) {
+   return dataStore.trans;
+}
+
+
+//get transaction by type
+function getTransactionType (trans) {
+if (trans === 'DEPOSIT') return dataStore.filter(cur => cur.TransactionType === trans);
+if(trans === 'WITHDRAWAL') return dataStore.filter(cur => cur.TransactionType === trans);
+}
+//console.log(getTransactionType('DEPOSIT'));
+
+
+
